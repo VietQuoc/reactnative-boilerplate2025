0c68f62129402825ea383e5feb5774442b2dd4ca
Quoc Nguyen <quoc.nguyen@acorai.com>
2025-02-09 19:29:29 +0700

    boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
 import 'react-native-gesture-handler';
 
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from '@/theme';
 import ApplicationNavigator from '@/navigation/Application';
 import { MMKV } from 'react-native-mmkv';
 
-// import '@/translations';
+import '@/translations';
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    mutations: {
+      retry: false,
+    },
+    queries: {
+      retry: false,
+    },
+  },
+});
 
 export const storage = new MMKV();
 
 function App() {
   return (
     <GestureHandlerRootView>
-      <ThemeProvider storage={storage}>
-        <ApplicationNavigator />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider storage={storage}>
+          <ApplicationNavigator />
+        </ThemeProvider>
+      </QueryClientProvider>
     </GestureHandlerRootView>
   );
 }
